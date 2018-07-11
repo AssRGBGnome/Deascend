bd329c8173f11c39394bcf1005493bc500f93dc4
Brandon Baker <assgnomergb@gmail.com>
2018-07-11 00:04:49 -0700

    First Submit!

---
+++ Assets/Standard Assets/2D/Scripts/Platformer2DUserControl.cs
@@ -0,0 +1,36 @@
+using System;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine.SceneManagement;
+
+public class Platformer2DUserControl : MonoBehaviour
+{
+    public PlatformerCharacter2D m_Character;
+    private bool m_Jump;
+    public float nonmovejumpforce;
+
+    private void Update()
+    {
+        if (!m_Jump)
+        {
+            float h = CrossPlatformInputManager.GetAxisRaw("Horizontal");
+            m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
+            if (!Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D) && h != 1 && h != -1)
+            {
+                m_Character.m_JumpForce = nonmovejumpforce;
+            }
+            else if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D) && h >= 1)
+            {
+                m_Character.m_JumpForce = 12000;
+            } //8000
+            else { m_Character.m_JumpForce = 12000; } //8000
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        float h = CrossPlatformInputManager.GetAxisRaw("Horizontal");
+        m_Character.Move(h, m_Jump);
+        m_Jump = false;
+    }
+}
---
+++ Assets/Standard Assets/2D/Scripts/PlatformerCharacter2D.cs
@@ -0,0 +1,63 @@
+using System;
+using UnityEngine;
+
+public class PlatformerCharacter2D : MonoBehaviour
+{
+    [SerializeField] public float m_MaxSpeed = 10f;
+    [SerializeField] public float m_JumpForce = 900f;
+    [SerializeField] private bool m_AirControl = false;
+    [SerializeField] private LayerMask m_WhatIsGround;
+    public Transform m_GroundCheck;
+    public Transform m_CeilingCheck;
+    public Rigidbody2D m_Rigidbody2D;
+    public bool m_Grounded;
+    private bool m_FacingRight = true;
+    const float k_GroundedRadius = .2f;
+    const float k_CeilingRadius = .01f;
+    public float lowjumpmultiplier;
+    public float maxVel = 60; //50
+    public float strechtimer = .1f;
+
+    private void FixedUpdate()
+    {
+        //if(m_Rigidbody2D.velocity.magnitude > maxVel)
+        //{
+        //    Debug.Log(m_Rigidbody2D.velocity.magnitude);
+        //    m_Rigidbody2D.velocity = m_Rigidbody2D.velocity.normalized * maxVel;
+        //}
+        m_Grounded = false;
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(m_GroundCheck.position, k_GroundedRadius, m_WhatIsGround);
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].gameObject != gameObject)
+            m_Grounded = true;
+        }
+        if (m_Rigidbody2D.velocity.y > 0 && !Input.GetButton("Jump"))
+        {
+            m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce *-(lowjumpmultiplier) * Time.deltaTime));
+        }
+    }
+
+    public void Move(float move, bool jump)
+    {
+        if (m_Grounded || m_AirControl)
+        {
+            m_Rigidbody2D.velocity = new Vector2(move * m_MaxSpeed, m_Rigidbody2D.velocity.y);
+            if (move > 0 && !m_FacingRight) { Flip(); }
+            else if (move < 0 && m_FacingRight) { Flip(); }
+        }
+        if (m_Grounded && jump)
+        {
+            m_Grounded = false;
+            m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
+        }
+    }
+
+    private void Flip()
+    {
+        m_FacingRight = !m_FacingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
