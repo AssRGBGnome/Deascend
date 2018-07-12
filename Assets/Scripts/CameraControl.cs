c58dc80bbfadb89a5f146240baf6121f6372473a
Brandon Baker <assgnomergb@gmail.com>
2018-07-12 03:18:32 -0700

    three levels, camera control, and sound effects (jumping)

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public Animator CamAnim;
+    public GameObject Player;
+    private Vector3 _Player;
+    private int level = 1;
+
+    private void Update()
+    {
+        _Player = Player.transform.position;
+        CameraChange();
+    }
+
+    private void CameraChange()
+    {
+        if (_Player.x >= -35.6 && _Player.x <= 35.6 && _Player.y >= -20 && _Player.y <= 20)
+        {
+            Debug.Log("Farts and stuff are cool tyo! You in 1.");
+            CamAnim.SetInteger("Level", 1);
+            level = 1;
+        }
+        else if(_Player.x >= 35.6 && _Player.x <= 106.7 && _Player.y >= -20 && _Player.y <= 20)
+        {
+            Debug.Log("Farts and stuff are cool tyo! You in 2.");
+            CamAnim.SetInteger("Level", 2);
+            level = 2;
+        }
+        else if(_Player.x >= 106.7 && _Player.x <= 177.8 && _Player.y >= -20 && _Player.y <= 20)
+        {
+            Debug.Log("Farts and stuff are cool tyo! You in 3.");
+            CamAnim.SetInteger("Level", 3);
+            level = 3;
+        }
+    }
+}
---
+++ Assets/Scripts/SoundController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundController : MonoBehaviour
+{
+    public AudioSource Audiorend;
+    public AudioClip[] Jumpsounds;
+    public PlatformerCharacter2D Platchar;
+    
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && Platchar.m_Grounded == true)
+        {
+            int j = Random.Range(0, Jumpsounds.Length);
+            Audiorend.clip = Jumpsounds[j];
+            Audiorend.Play();
+            Debug.Log(j);
+        }
+    }
+}
