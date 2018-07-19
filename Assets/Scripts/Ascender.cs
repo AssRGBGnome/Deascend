6ce157c32b0a3cee15bf9813d26494386107c99a
Brandon Baker <assgnomergb@gmail.com>
2018-07-18 22:22:24 -0700

    level 2, animations, bubble, and more

--- Assets/Scripts/Ascender.cs
+++ Assets/Scripts/Ascender.cs
@@ -7,26 +7,44 @@ public class Ascender : MonoBehaviour
     public GameObject asdfdsasd;
     public CameraControl CamrController;
     public SoundController SoundC;
-    public Animator Ascend;
+    public Animator Ascend1;
+    public Animator Ascend2;
+    public int level = 1;
     public int floor = 1;
 
     private void Update()
     {
-        if (CamrController.level == 1)
+        level = CamrController.level;
+        if (level == 1)
         {
             if (Input.GetKey(KeyCode.E) && floor != 2)
             {
                 floor = 2;
-                Ascend.SetInteger("floor", 2);
+                Ascend1.SetInteger("floor", 2);
                 SoundC.PlayAscendSound();
                 //asdfdsasd.transform.position += new Vector3(0, 5, 0);
             }
             if (Input.GetKey(KeyCode.Q) && floor != 1)
             {
                 floor = 1;
-                Ascend.SetInteger("floor", 1);
+                Ascend1.SetInteger("floor", 1);
                 SoundC.PlayDescendSound();
                 //asdfdsasd.transform.position += new Vector3(0, -5, 0);
+            }
+        }
+        if(level == 2)
+        {
+            if(Input.GetKey(KeyCode.E) && floor != 2)
+            {
+                floor = 2;
+                Ascend2.SetInteger("floor", 2);
+                SoundC.PlayAscendSound();
+            }
+            if (Input.GetKey(KeyCode.Q) && floor != 1)
+            {
+                floor = 1;
+                Ascend2.SetInteger("floor", 1);
+                SoundC.PlayDescendSound();
             }
         }
     }
--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -5,18 +5,28 @@ using UnityEngine;
 public class PlayerCollision : MonoBehaviour
 {
     public Transform[] Levelskipositions;
-    public GameObject Bubble;
-    public Animator A_Bubble;
+    public GameObject[] Bubble;
+    public AudioSource Music;
+    public AudioSource[] SFX;
+    public Animator[] A_Bubble;
     public CameraControl CameraCon;
     private int currentlevel = 1;
-    private bool mute = true;
+    private bool muteMusic = false;
+    private bool muteSFX = false;
 
     private void Update()
     {
         //to mute on default for builds
-        if(mute == true)
+        if(muteMusic == true)
         {
-            AudioListener.volume = 0;
+            Music.mute = true;
+        }
+        if(muteSFX == true)
+        {
+            for (int i = 0; i < SFX.Length; i++)
+            {
+                SFX[i].mute = true;
+            }
         }
         Cursor.visible = false;
         currentlevel = CameraCon.level;
@@ -40,23 +50,45 @@ public class PlayerCollision : MonoBehaviour
         }
         if (Input.GetKeyDown(KeyCode.M))
         {
-            mute = !mute;
-            if(mute == true)
+            muteMusic = !muteMusic;
+            if(muteMusic == true)
             {
-                AudioListener.volume = 0;
+                Music.mute = true;
             }
-            if(mute == false)
+            if(muteMusic == false)
             {
-                AudioListener.volume = 1;
+                Music.mute = false;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            muteSFX = !muteSFX;
+            if(muteSFX == true)
+            {
+                for (int i = 0; i < SFX.Length; i++)
+                {
+                    SFX[i].mute = true;
+                }
+            }
+            if(muteSFX == false)
+            {
+                for (int i = 0; i < SFX.Length; i++)
+                {
+                    SFX[i].mute = false;
+                }
             }
         }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.gameObject == Bubble)
+        if(other.gameObject == Bubble[0])
         {
-            A_Bubble.SetBool("GetBubble", true);
+            A_Bubble[0].SetBool("GetBubble", true);
+        }
+        if(other.gameObject == Bubble[1])
+        {
+            A_Bubble[1].SetBool("GetBubble", true);
         }
     }
 }
